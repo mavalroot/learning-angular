d566913b0ba43ba415f08a392a8bc70849ae3aa8
mavalroot <mavalrodev@gmail.com>
2018-10-18 17:23:34 +0200

    Finished assignment

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+    userName: string = '';
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    resetUsername() {
+        this.userName = '';
+    }
+
+    isUserNameEmpty() {
+        return this.userName == '';
+    }
+
+}
