e7612ce3ceb5490a3bd763d7b8762542e727438f
mavalroot <mavalrodev@gmail.com>
2018-10-18 15:33:23 +0200

    showing off properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ExampleComponent } from './example/example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule
