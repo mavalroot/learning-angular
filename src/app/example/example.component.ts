35f7bb2d982c7107ceb3dbac1fb32a15be842f97
mavalroot <mavalrodev@gmail.com>
2018-10-18 16:51:45 +0200

    using events

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-example',
-  templateUrl: './example.component.html',
-  styleUrls: ['./example.component.css']
+    selector: 'app-example',
+    templateUrl: './example.component.html',
+    styleUrls: ['./example.component.css']
 })
 export class ExampleComponent {
-  example1: number = 5;
-  example2: string = 'how are you';
 
-  getExample1() {
-      return this.example1;
-  }
+    allowNewExample = false;
+    exampleCreationStatus = 'No example was created!';
+
+    example1: number = 5;
+    example2: string = 'how are you';
+    exampleName: string = 'example';
+
+    constructor() {
+        setTimeout(() => {
+            this.allowNewExample = true;
+        }, 2000);
+    }
+
+    ngOnInit() {
+    }
+
+    getExample1() {
+        return this.example1;
+    }
+
+    onCreateExample() {
+        this.exampleCreationStatus = 'Server was created!'
+    }
+
+    onUpdateExampleName(event: Event) {
+        this.exampleName = (<HTMLInputElement>event.target).value;
+    }
 }
